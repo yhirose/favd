f8fb0b8b34775d5a5e0ff9578835810c90d7fe46
yhirose <yuji.hirose.bug@gmail.com>
2010-08-11 23:45:10 -0400

    first commit

---
+++ favd.sh
@@ -0,0 +1,87 @@
+#
+# favd.sh - favorite directories shell script
+#
+# auther: Yuji Hirose <yuji.hirose.bug at gmail.com>
+# license: New BSD License
+#
+# usage:
+#   favd add    - add the current directory to the path list
+#   favd NUMBER - jump to the selected path by number
+#   favd del    - delete a path from the path list
+#   favd edit   - open the path list
+#
+# note: .favd file contains your favorite paths.
+#
+
+function favd_select() {
+    # create .favd file if it doesn't exist.
+    if ! [ -f ~/.favd ]; then
+        echo "" > ~/.favd
+    fi
+
+    # read paths
+    local paths=""
+    while read path; do
+        path=${path// /%20}
+        paths="$paths $path"
+    done < ~/.favd
+
+    # select path
+    select favd_path in $paths; do
+        favd_path=${favd_path//%20/\\ }
+        break
+    done
+}
+
+function favd() {
+    if [ -n "$1" ]; then
+        # add the current path
+        if [ $1 = "add" ]; then
+            local curd=`pwd`
+            local rtn=`grep -c -e "^$curd$" ~/.favd`
+            if [ $rtn = "0" ]; then
+                echo "add $curd..."
+                echo $curd >> ~/.favd
+            fi
+
+        # delete by number
+        elif [ $1 = "del" ]; then
+            favd_select
+            if [ -n "$favd_path" ]; then
+                sed -e "/${favd_path//\//\\/}/d" ~/.favd > ~/.favd_tmp
+                rm -f ~/.favd
+                mv ~/.favd_tmp ~/.favd
+            fi
+
+        # edit .favd
+        elif [ $1 = "edit" ]; then
+            # (1) alternatives symbolic file system
+            if [ -f `which editor` ]; then
+                editor ~/.favd
+            # (2) EDITOR environment variable
+            elif [ -n "$EDITOR" ]; then
+                $EDITOR ~/.favd
+            # (3) vi
+            else
+                vi ~/.favd
+            fi
+
+        # jump by number
+        elif [ $(($1)) != 0 ]; then
+            sed -e "$1p" -e "d" ~/.favd > ~/.favd_tmp
+            read path < ~/.favd_tmp
+            rm -f ~/.favd_tmp
+            cd $path
+
+        # show usage
+        else
+            echo "usage: favd [<number> | add | del | edit]"
+        fi
+    else
+        # jump with selection
+        favd_select
+        if [ -n "$favd_path" ]; then
+            eval cd $favd_path
+        fi
+    fi
+}
